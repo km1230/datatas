a0f650b60541802577ba159e3db1f6979dff1ff9
Kavie <moklavie@gmail.com>
2018-06-04 14:44:13 +1000

    3rd print not None

--- shapes.py
+++ shapes.py
@@ -2,3 +2,13 @@
 
 #define a list of valid shapes
 valid_shapes = [None, None, None, 'triangle', 'square','pentagon', 'hexagon']
+
+#first function
+def shape(sides):
+	side = int(sides)
+	return valid_shapes['side']
+
+print('I know about the following shape')
+for s in valid_shapes:
+		if s is not None:
+			print(s)
